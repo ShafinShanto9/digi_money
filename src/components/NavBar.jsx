bdd568b9983ef7b9540612b6137854fe5b1c87b5
shafin_shant0 <shafinshanto9@gmail.com>
2022-06-28 14:03:23 +0600

    complet navbar structure

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,47 @@
+import { motion } from "framer-motion"
+import { AiOutlineMenu } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 import ThemeToggle from './ThemeToggle'
 
 const NavBar = () => {
   return (
-    <div>
+    <div className="rounded-div flex items-center justify-between h-20 font-bold">
       <Link to="/">
-          <h1>DIGI-MONEY</h1>
+        <motion.div
+          whileTap={{ scaleY: 0.5 }}>
+          <h1 className="text-2xl">DIGI-MONEY</h1>
+        </motion.div>
       </Link>
-      <div>
+      <div className="hidden md:block">
         <ThemeToggle/>
+      </div>
+      <div className="hidden md:block">
+        <Link to="/signin">Sign-In</Link>
+        <Link to="/signup">Sign-Up</Link>
+      </div>
+      {/* Menu Icon */}
+      <div>
+        <AiOutlineMenu/>
+      </div>
+      {/* Mobile Menu */}
+      <div >
+        <div><ThemeToggle/></div>
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/account">Account</Link>
+          </li>  
+        </ul>
+        <div>
+          <Link to="/signin">
+            <button>Sign-In</button>
+          </Link>
+          <Link to="/signup">
+            <button>Sign-Up</button>
+          </Link>
+        </div>
       </div>
     </div>
   )
--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import { useContext } from "react"
 import { HiMoon, HiSun } from "react-icons/hi"
 import { ThemeContext } from "../context/ThemeContext"
@@ -5,14 +6,26 @@ import { ThemeContext } from "../context/ThemeContext"
 const ThemeToggle = () => {
     const {theme, setTheme} = useContext(ThemeContext)
   return (
-      <div>
+      <div whileTap={{scale:0.6}} className="p-2 ">
       {theme === 'dark' ? (
-        <div className="cursor-pointer" onClick={()=>setTheme(theme === "dark" ? "light": "dark")}>
-          <HiSun className='text-primary text-2xl mr-2' /> Light Mode
+        <div className="cursor-pointer flex items-center" onClick={()=>setTheme(theme === "dark" ? "light": "dark")}>
+          <motion.div whileTap={{
+            scale: 0.8,
+            rotate: -90,
+            duration: 3
+          }}>
+            <HiSun className='text-primary text-2xl mr-2' />
+          </motion.div> Light Mode
         </div>
       ) : (
-        <div className="cursor-pointer" onClick={()=>setTheme(theme === "dark" ? "light": "dark")}>
-          <HiMoon className='text-primary text-2xl mr-2' /> Dark Mode
+        <div className="cursor-pointer flex items-center" onClick={()=>setTheme(theme === "dark" ? "light": "dark")}>
+          <motion.div whileTap={{
+            scale: 0.8,
+            rotate: -90,
+            duration: 3
+          }}>
+            <HiMoon className='text-primary text-2xl mr-2' />
+          </motion.div> Dark Mode
         </div>
       )}
     </div>
