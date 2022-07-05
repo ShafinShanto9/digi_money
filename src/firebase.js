5aa76ffc2a5640b2e1816ba4bffac33cc1ffc73f
shafin_shant0 <shafinshanto9@gmail.com>
2022-07-05 12:53:07 +0600

    adding firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFLm_1q6NC9JKxXgAyrN55n_R6J4k_TmU",
+  authDomain: "digimoney-6c616.firebaseapp.com",
+  projectId: "digimoney-6c616",
+  storageBucket: "digimoney-6c616.appspot.com",
+  messagingSenderId: "548168300670",
+  appId: "1:548168300670:web:b47eed5b69b5e2acb2402f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -12,7 +12,7 @@ const Account = () => {
           </div>
         </div>
         <div>
-          <motion.button className='border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl'>Sign Out</motion.button>
+          <motion.button whileTap={{scale: 0.5}} className='border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl hover:bg-red-400 hover:text-white '>Sign Out</motion.button>
         </div>
       </div>
       <div flex justfiy-between items-center my-12 py-8 rounded-div>
